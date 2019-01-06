b195bc3570ca4f9da823c929fe34fd6ad727f2f6
catmblake <catmblake@gmail.com>
2019-01-06 14:08:55 -0800

    initial site files and setup for database and handlebars

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS `restaurantDb`;
+CREATE DATABASE `restaurantDb`;
+USE `restaurantDb`;
---
+++ models/favorite.js
@@ -0,0 +1,8 @@
+module.exports = function (sequelize, DataTypes) {
+    var Favorite = sequelize.define("Favorite", {
+        name: DataTypes.STRING,
+        image_url: DataTypes.TEXT,
+        page_url: DataTypes.STRING
+    });
+    return Favorite;
+}
